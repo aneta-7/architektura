0a38debf27c250aaf20ba1fccd0647b0449b479d
aneta-7 <aneta.stypa7@wp.pl>
2016-04-01 15:22:52 +0200

    create user

--- Shop2/db/schema.rb
+++ Shop2/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160316113600) do
+ActiveRecord::Schema.define(version: 20160401125534) do
 
   create_table "ads", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20160316113600) do
     t.string   "url_img"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
