76f4d9e5ee6648abf4f2384ac07d335e19ea228e
aneta-stypa <aneta.stypa7@wp.pl>
2016-04-21 12:58:04 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ end
 99.times do |n|
 	Ad.create(
         name: Faker::Commerce.product_name,
-        description: Faker::Lorem.characters(80),
+        description: Faker::Lorem.sentences(80),
         price: Faker::Number.decimal(2),
         seller_id: Faker::Number.between(0,99),
         email: Faker::Internet.email,
